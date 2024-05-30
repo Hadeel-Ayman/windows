5c834da5a4650d29baf0159c9e4022f1827dae0f
Hadeel-Ayman <haddelayman4@gmail.com>
2024-05-31 00:26:40 +0300

    add change

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -59,19 +59,19 @@ exports.isAdminAuth = expressAsyncHandler(async (req, res, next) => {
 exports.isSuperAdminAuthenticated = expressAsyncHandler(async (req, res, next) => {
     const token = req.headers.authorization;
     if (!token) {
-        return next(new ErrorHandler("User is not authenticated!", 400));
+        return next(new ApiError("User is not authenticated!", 400));
     }
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
         req.user = await User.findById(decoded.id);
         if (req.user.role !== "SuperAdmin") {
             return next(
-                new ErrorHandler(`${req.user.role} not authorized for this resource!`, 403)
+                new ApiError(`${req.user.role} not authorized for this resource!`, 403)
             );
         }
         next();
     } catch (error) {
-        return next(new ErrorHandler("Invalid token or user not found!", 401));
+        return next(new ApiError("Invalid token or user not found!", 401));
     }
 }
 );
